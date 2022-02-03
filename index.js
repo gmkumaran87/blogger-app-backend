a48d98802dcf2eb3f319d56e018cacf5ce6c8836
Muthukumaran <gmkumaran87@gmail.com>
2022-02-03 14:16:09 +0530

    CORS request enabled in index.js

--- index.js
+++ index.js
@@ -23,6 +23,15 @@ const catRouter = require("./routers/category");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+// Setting CORS requests
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(helmet());
